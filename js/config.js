0b3c2c25d220fef8c6e8db05f9d4ba4e649ebf31
theerakankh-collab <theerakan.kh@signalschool.ac.th>
2026-07-27 03:37:22 +0700

    Refactor Supabase configuration in config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,11 @@
-const SUPABASE_URL="https://YOUR_PROJECT.supabase.co";
+// =======================================
+// config.js
+// =======================================
 
-const SUPABASE_KEY="YOUR_ANON_KEY";
+const SUPABASE_URL = "https://YOUR_PROJECT.supabase.co";
+const SUPABASE_KEY = "YOUR_ANON_KEY";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
