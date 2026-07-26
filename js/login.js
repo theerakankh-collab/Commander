d6bd0ef3dd4cda52ed58a58ce80b517922397e16
theerakankh-collab <theerakan.kh@signalschool.ac.th>
2026-07-26 17:12:10 +0700

    Add login functionality with Supabase authentication

--- js/login.js
+++ js/login.js
@@ -1,1 +1,33 @@
+document
+.getElementById("loginBtn")
+.addEventListener("click", login);
 
+async function login(){
+
+const email =
+document.getElementById("email").value;
+
+const password =
+document.getElementById("password").value;
+
+const {error} =
+await supabase.auth.signInWithPassword({
+
+email,
+
+password
+
+});
+
+if(error){
+
+document.getElementById("msg").innerHTML=
+error.message;
+
+return;
+
+}
+
+location.href="dashboard.html";
+
+}
