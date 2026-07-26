9251c2ad0695141195dade3d6f128d1dda08f446
theerakankh-collab <theerakan.kh@signalschool.ac.th>
2026-07-26 15:37:56 +0700

    Create tables for personnel, commanders, and wives

--- sql/database.sql
+++ sql/database.sql
@@ -1,1 +1,51 @@
+create table personnel (
 
+id bigint generated always as identity primary key,
+
+rank varchar(30),
+
+firstname varchar(100),
+
+lastname varchar(100),
+
+nickname varchar(100),
+
+position text,
+
+class varchar(20),
+
+phone varchar(30),
+
+remark text,
+
+image text,
+
+created_at timestamp default now()
+
+);
+
+create table commanders(
+
+id bigint generated always as identity primary key,
+
+rank varchar(30),
+
+firstname varchar(100),
+
+lastname varchar(100),
+
+position text
+
+);
+
+create table wives(
+
+id bigint generated always as identity primary key,
+
+personnel_id bigint,
+
+wife_name text,
+
+phone varchar(30)
+
+);
