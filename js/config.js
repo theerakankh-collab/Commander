193b134fcca0042e8ab592fc28ac5bf1f143e623
theerakankh-collab <theerakan.kh@signalschool.ac.th>
2026-07-28 14:00:54 +0700

    Modify SUPABASE_URL in config.js
    Updated SUPABASE_URL to remove the '/rest/v1/' endpoint.

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // config.js
 // =======================================
 
-const SUPABASE_URL = "https://sftbzmwrrymlgbsmuima.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://sftbzmwrrymlgbsmuima.supabase.co";
 const SUPABASE_KEY = "sb_publishable_J3UrucI0x66JEbHUQQHlXA_QFQf-ioz";
 
 const supabase = window.supabase.createClient(
